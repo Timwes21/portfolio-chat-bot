c549b1c47ced5c1ea5ed5caa6396314a97e59e2f
Timothy Wesley <timothy.wesley99@yahoo.com>
2025-05-19 00:16:44 -0400

    added the current date

--- agent.py
+++ agent.py
@@ -2,21 +2,19 @@ from openai import OpenAI
 import os
 from dotenv import load_dotenv
 load_dotenv()
-
+import datetime
 api_key = os.getenv("OPEN_AI_API_KEY")
 vector_id = os.getenv("VECTOR_ID")
-
 
 client = OpenAI(api_key=api_key)
 
 
 
-def get_agent_reply(user_message):
-    
+def get_agent_reply(user_message):    
     input = [
         {
             "role": "developer",
-            "content": "You are being used in my portfolio to mimic me, try to act like me and answer questions about me as if you are me, never break character and try to keep response short"
+            "content": "You are being used in my portfolio to mimic me, try to act like me and answer questions about me as if you are me, never break character and try to keep response short. And for reference todays date is " + datetime.date.today()
         },
         {
             "role": "user",
@@ -37,6 +35,4 @@ def get_agent_reply(user_message):
     
     return response.output_text
 
-reply = get_agent_reply("how old are you")
-print(reply)
     
